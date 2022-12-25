fc8be6d0cdf7fa1b7e7f18076f835f8a2f40795f
gautaz <11046232+gautaz@users.noreply.github.com>
2022-12-25 13:55:19 +0100

    initial revision with testbox

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Flake for managing Nix[OS] configuration";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+  };
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    lib = nixpkgs.lib;
+  in {
+    nixosConfigurations = {
+      testbox = lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./systems/testbox/configuration.nix
+        ];
+      };
+    };
+  };
+}
